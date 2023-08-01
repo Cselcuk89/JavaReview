607c04b024a7f107ae5c6b77f8e2092cdd0280e7
Cselcuk89 <cagriselcuk89@gmail.com>
2023-08-01 00:13:33 -0500

    initial commit

---
+++ generics/src/main/java/org/cydeoSelcuk/TeamClass.java
@@ -0,0 +1,30 @@
+package org.cydeoSelcuk;
+
+import java.util.ArrayList;
+
+public class TeamClass<T extends Player> {
+    private String name;
+    private ArrayList<T> members = new ArrayList<>();
+
+    public TeamClass(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public boolean addPlayer(T player){
+        if (members.contains(player)){
+            System.out.println(player.getName() + " is already on this team");
+            return  false;
+        }else {
+            members.add(player);
+            System.out.println(player.getName() + " is picked for team" + this.name);
+            return  true;
+        }
+    }
+    public int numPlayer(){
+        return this.members.size();
+
+    }
+}
